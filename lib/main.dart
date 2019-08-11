d1d85498ff5619db404ca5ce3b7c96f5fa88fa43
shrinidhikatti <katti.shrinidhi@gmail.com>
2019-08-11 19:04:33 +0530

    1.1

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'scratch and win! play and enjoy ',
+      title: 'scratch and win amount! ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
