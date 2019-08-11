ce50a6f3a7e3f2342607a2d224bf48b35d0bd521
shrinidhikatti <katti.shrinidhi@gmail.com>
2019-08-11 18:41:16 +0530

    Beta

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+
+void main()=> runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'scratch and win',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+
+    );
+  }
+}
